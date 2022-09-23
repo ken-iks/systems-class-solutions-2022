89e7b3fe64db5564ec4f53b64774f4dbd179f815
Kenny Ikeji <kikeji@college.harvard.edu>
2022-09-23 10:41:58 -0400

    Done./cs61-run-docker Phase 1-3 over

--- pset1/m61.cc
+++ pset1/m61.cc
@@ -11,6 +11,8 @@
 static void * m61_find_free_space(size_t, size_t);
 const size_t SIZECONST = sizeof('#');
 
+// # is my special character that I have at the end of every allocation
+// That is for checking for boundary-write errors
 
 struct m61_memory_buffer {
     char* buffer;
@@ -65,14 +67,17 @@ std::map<size_t, size_t> freed_allocations;
 using freemap_iter = std::map<size_t, size_t>::iterator;
 std::map<size_t, size_t> active_allocations;
 
+// Extra info stores the file and line for a given active allocation
 struct extra_info {
     const char* file;
     int line;
 };
 
+// Map for storing some extra metadata for active allocations
 std::map<size_t, extra_info> allocation_info;
 using extramap_iter = std::map<size_t, extra_info>::iterator;
 
+// Function which computes the amount of padding that needs to be added
 size_t add_padding(size_t sz) {
     size_t pad = alignof(max_align_t) - (sz % alignof(max_align_t));
     if (pad == alignof(max_align_t)) {
@@ -83,6 +88,7 @@ size_t add_padding(size_t sz) {
 
 // Initially setting up the freed allocation to have the 8Mib buffer
 // This is to make it easy to coalesce never-freed memory
+// Assertions are to ensure alignments are correct
 
 int setup() {
     while (default_buffer.pos % alignof(max_align_t) != 0) {
@@ -91,12 +97,9 @@ int setup() {
     assert(default_buffer.pos % 16 == 0);
     size_t starting_pos = (size_t) &default_buffer.buffer[default_buffer.pos];
     assert(starting_pos % 16 == 0);
-    assert((8<<20) % 16 == 0);
     freed_allocations.insert({starting_pos, (8<<20)});
     auto testing2 = (char*) (starting_pos + (8<<20) - SIZECONST);
-    //printf("%p is where I am putting my special character\n", testing2);
     *testing2 = '#';
-
     return 0;
 }
 
@@ -105,14 +108,12 @@ void check_freed() {
     for (auto p = freed_allocations.begin();
     p != freed_allocations.end();
     p++) {
-        //printf("%zx is of size %zx\n", p->first, p->second);
-        //printf("%zx mod 16 = %li\n", p->first, p->first%alignof(max_align_t));
+        printf("%zx is of size %zx\n", p->first, p->second);
+        printf("%zx mod 16 = %li\n", p->first, p->first%alignof(max_align_t));
     }
 }
 
 auto starter = setup();
-
-
 
 // Function to update statistics every time a new malloc is performed
 
@@ -131,12 +132,18 @@ static void new_malloc(void* ptr, size_t sz, const char* file, int line) {
         my_data.smallest_bytes_location = (uintptr_t) ptr;
     }
 
+    // Update active allocations with original information
     active_allocations[(size_t) ptr] = sz + SIZECONST;
     allocation_info[(size_t) ptr] = {file, line};
+
+    // Space_im_taking is the actual size that the malloc has
+    // This is original size + the size of our special character
+    // We then add padding to it for alignement
     size_t space_im_taking = sz + SIZECONST;
     space_im_taking += add_padding(space_im_taking);
 
     // Checking if the malloc is on the original buffer
+    // If so we may need to break it up into smaller pieces
     auto it = freed_allocations.find((size_t) ptr);
     if (it != freed_allocations.end()) {
         size_t spare_space = it->second - space_im_taking;
@@ -182,9 +189,7 @@ void coalesce_up(freemap_iter it) {
     ++next;
     it->second += next->second;
     auto testing2 = (char*) (it->first + it->second - SIZECONST);
-    //printf("%p is where I am putting my special character for coalesce\n", testing2);
     *testing2 = '#';
-    //printf("%i is in %p\n", *testing2, testing2);
     freed_allocations.erase(next);
 }
 
@@ -203,26 +208,14 @@ static void * m61_find_free_space(size_t sz, size_t partial_size) {
             void* ptr = (void*) p->first;
             // Spare memory in the block that will not be used
             size_t spare_space = p->second - sz;
-            //printf("Checking alignement of %zx\n", p->first);
             assert(p->first % 16 == 0);
-            //printf("%zx is aligned!!!\n", p->first);
             assert(p->second % 16 == 0);
-            /*auto testing = (int*) (p->first + sz + spare_space - SIZECONST);
-            printf("%p is where I am putting my special character\n", testing);
-            *testing = 4;*/
-           assert(spare_space % 16 == 0);
-           assert(sz % 16 == 0);
-           //assert((p->first + sz - SIZECONST) % 16 == 0);
+            assert(spare_space % 16 == 0);
+            assert(sz % 16 == 0);
             if (spare_space > 0) {
-                //printf("time to work!!! Pointer to %zx of size %zu with %zu spare\n", p->first, p->second, spare_space);
-                //size_t pad = alignof(max_align_t) - ((spare_space) % alignof(max_align_t));
-                //spare_space+=add_padding(spare_space);
-                //printf("Now the spare is %zu\n", spare_space + pad);
-                //printf("uh ohhh\n");
                 freed_allocations[p->first + sz] = spare_space ;
             }
             auto testing2 = (char*) (p->first + partial_size);
-            //printf("%p is where I am putting my special character\n", testing2);
             *testing2 = '#';
             freed_allocations.erase(p->first);
             return ptr;               
@@ -230,20 +223,6 @@ static void * m61_find_free_space(size_t sz, size_t partial_size) {
     }
     return nullptr;
 }
-
-void* coalesce_with_buffer(size_t ptr) {
-    for (auto p = freed_allocations.begin();
-        p != freed_allocations.end();
-        p++) {
-            //printf("Maybe??");
-            size_t end_of_block = p->first + p->second + SIZECONST;
-            if (end_of_block == ptr) {
-                return (void*) p->first;
-            }
-        }
-    return nullptr;
-}
-
 
 /// m61_malloc(sz, file, line)
 ///    Returns a pointer to `sz` bytes of freshly-allocated dynamic memory.
@@ -263,22 +242,18 @@ void* m61_malloc(size_t sz, const char* file, int line) {
         return nullptr;
     }   
 
-    //("%zu is orginal size\n", sz);
-
     size_t whole_size = sz + SIZECONST;
     whole_size+=add_padding(whole_size);
-
-    //printf("%zu is padded size\n", whole_size);
 
     if (default_buffer.pos + whole_size > default_buffer.size 
         || SIZE_MAX - sz < default_buffer.pos) {
 
-            //printf("oh damnnn\n");
         // Not enough space left in default buffer for allocation
         // Or check for integer overflow
 
         // Look up allocation sizes to see if it can fit into a freed spot
-        // Currently rechecking defult buffer but should find a better way
+        // Only do this if it wasn't integer overflow
+
         if (default_buffer.pos + whole_size > default_buffer.size) {
             assert(whole_size % 16 == 0);
             void* pot_ptr = m61_find_free_space(whole_size, sz);
@@ -310,19 +285,13 @@ void* m61_malloc(size_t sz, const char* file, int line) {
 
     assert(default_buffer.pos % 16 == 0);
 
-    // Reserve some more space
-    // Put something there (not zero) -> use ptr (start of block location) + size - to get to the end of the block and start your extra thing
-    // Inside free, check if the value at the end was still there or if it was changed
-    // So basically we are letting them do it then we are chekcing if they did it
-
+    // Adding special character to the end of buffer
     default_buffer.buffer[default_buffer.pos + sz] = '#';
 
     default_buffer.pos += total_size;
 
     assert(default_buffer.pos % 16 ==0);
     
-
-
     new_malloc(ptr, sz, file, line);
     return ptr;
 }
@@ -344,49 +313,57 @@ void m61_free(void* ptr, const char* file, int line) {
         auto it1 = active_allocations.find(ptr_pos);
         auto it2 = freed_allocations.find(ptr_pos);
 
-
-        check_freed();
-
-
-
-        // Check if allocation actually exists
+        // Check if allocation actually exists - if not raise some errors
         if (ptr_pos > my_data.largest_bytes_location 
         || ptr_pos < my_data.smallest_bytes_location) {
-            fprintf(stderr, "MEMORY BUG: %s:%d: invalid free of pointer %p, not in heap\n", file, line, ptr);
+            fprintf(stderr, 
+                    "MEMORY BUG: %s:%d: invalid free of pointer %p, not in heap\n", 
+                    file, line, ptr);
             abort();             
         }
         if (it2 != freed_allocations.end()) {
-            fprintf(stderr, "MEMORY BUG: %s:%d: invalid free of pointer %p, double free\n", file, line, ptr);
+            fprintf(stderr, 
+                    "MEMORY BUG: %s:%d: invalid free of pointer %p, double free\n", 
+                    file, line, ptr);
             abort();    
         }
         if (it1 == active_allocations.end()) {
-            fprintf(stderr, "MEMORY BUG: %s:%d: invalid free of pointer %p, not allocated\n", file, line, ptr);
+            fprintf(stderr, 
+                    "MEMORY BUG: %s:%d: invalid free of pointer %p, not allocated\n", 
+                    file, line, ptr);
 
             for (auto p = active_allocations.begin();
             p != active_allocations.end();
             p++) {
                 if (ptr_pos > p->first && ptr_pos < p->first + p->second) {
                     auto it = allocation_info.find(p->first);
-                    fprintf(stderr, "  %s:%d: %p is %zu bytes inside a %zu byte region allocated here\n", it->second.file, it->second.line, ptr, ptr_pos - p->first, p->second - SIZECONST);
+                    fprintf(stderr, 
+                            "  %s:%d: %p is %zu bytes inside a %zu byte region allocated here\n", 
+                            it->second.file, it->second.line, ptr, ptr_pos - p->first, p->second - SIZECONST);
                     abort();
                 }
             }
-
             abort();
         }
+        // Testing is checking to see if my special character is still at the end of the malloc
+        // If not then it was a wild write
         auto testing = (char*) (it1->first + it1->second - SIZECONST);
-        //printf("%p is the memory location of the special character\n", testing);
         if (*testing != '#') {
              printf("%p does not have #. Instead it has %i\n", testing, *(testing));
-             fprintf(stderr, "MEMORY BUG: %s:%d: detected wild write during free of pointer %p\n", file, line, ptr);
+             fprintf(stderr, 
+                    "MEMORY BUG: %s:%d: detected wild write during free of pointer %p\n", 
+                    file, line, ptr);
              abort();           
         }
         assert((ptr_pos + add_padding(ptr_pos)) % 16 == 0);
 
-        freed_allocations.insert({ptr_pos + add_padding(ptr_pos), it1->second + add_padding(it1->second)});
+        // Update statistics
+        freed_allocations.insert({ptr_pos + add_padding(ptr_pos), 
+                                    it1->second + add_padding(it1->second)});
         my_data.n_frees ++;
         my_data.freed_bytes += it1->second - SIZECONST;
 
+        // Coalesce
         auto it = freed_allocations.find(ptr_pos + add_padding(ptr_pos));
         while (can_coalesce_down(it)) {
             --it;
@@ -411,8 +388,6 @@ void* m61_calloc(size_t count, size_t sz, const char* file, int line) {
     // Your code here (to fix test019).
     if (SIZE_MAX / sz < count) {
         my_data.n_fails++;
-        // Question: In the case where there is integer overflow, what are
-        // you adding to the number of failed bytes?
         my_data.failed_bytes += sz;
         return nullptr;
     }
@@ -465,6 +440,7 @@ void m61_print_leak_report() {
     p != active_allocations.end();
     p++) {
         auto it = allocation_info.find(p->first);
-        printf("LEAK CHECK: %s:%d: allocated object %p with size %zu\n", it->second.file, it->second.line, (void*) p->first, p->second - SIZECONST);
+        printf("LEAK CHECK: %s:%d: allocated object %p with size %zu\n", 
+                it->second.file, it->second.line, (void*) p->first, p->second - SIZECONST);
     }
 }
